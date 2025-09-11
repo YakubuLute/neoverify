bc50197b6bbabda8c348e1fd9ca3505255a527f6
Yakubu Lute <yakubulute@outlook.com>
2025-09-11 06:55:31 +0000

    Files cleanup

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import { AppTopbar } from './components/app.topbar';
-import { StatsWidget } from "./components/dashboard/statswidget";
-import { SalesTrendWidget } from "./components/dashboard/salestrendwidget";
-import { RecentActivityWidget } from "./components/dashboard/recentactivitywidget";
-import { ProductOverviewWidget } from "./components/dashboard/productoverviewwidget";
-import { AppFooter } from "./components/app.footer";
+
 
 @Component({
   selector: 'app-root',
-  imports: [AppTopbar, StatsWidget, SalesTrendWidget, RecentActivityWidget, ProductOverviewWidget, AppFooter],
+  imports:[],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
