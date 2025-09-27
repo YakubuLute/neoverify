8ee836fba46aa2f02d965a6360d9ea3d87b010ef
Yakubu Lute <yakubulute@outlook.com>
2025-09-27 16:35:31 +0000

    Updates

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button'
+import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports:[ButtonModule],
+  imports: [ButtonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,15 +1,17 @@
-import { Routes } from '@angular/router';
-import { Landing  as HomePage} from './views/landing/landing';
-import { Login as LoginPage } from './views/auth/login/login';
-import { Register as RegisterPage } from './views/auth/register/register';
-import { Verify as VerifyPage } from './views/auth/verify/verify';
-import { Dashboard } from './views/dashboard/dashboard';
+import { Routes } from '@angular/router'
+import { Landing as HomePage } from './views/landing/landing'
+import { Login as LoginPage } from './views/auth/login/login'
+import { Register as RegisterPage } from './views/auth/register/register'
+import { Verify as VerifyPage } from './views/auth/verify/verify'
+import { Dashboard } from './views/dashboard/dashboard'
+import {Notfound} from './views/notfound/notfound'
 
 export const routes: Routes = [
-    {path: '', component: HomePage},
-    {path: 'login', component: LoginPage},
-    {path: 'register', component: RegisterPage},
-    {path: 'verify', component: VerifyPage},
-    {path: 'dashboard', component: Dashboard},
-
-];
+  { path: '', component: HomePage },
+  { path: 'login', component: LoginPage },
+  { path: 'register', component: RegisterPage },
+  { path: 'verify', component: VerifyPage },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'not-found', component: Notfound },
+  { path: '**', redirectTo: '/not-found' }
+]
