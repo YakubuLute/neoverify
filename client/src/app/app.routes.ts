0e36ea535682498883d233f594469b58fc462883
Yakubu Lute <yakubulute@outlook.com>
2025-09-27 14:15:55 +0000

    Created components for the varius views

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Landing  as HomePage} from './views/landing/landing';
+import { Login as LoginPage } from './views/auth/login/login';
+import { Register as RegisterPage } from './views/auth/register/register';
+import { Verify as VerifyPage } from './views/auth/verify/verify';
+import { Dashboard } from './views/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomePage},
+    {path: 'login', component: LoginPage},
+    {path: 'register', component: RegisterPage},
+    {path: 'verify', component: VerifyPage},
+    {path: 'dashboard', component: Dashboard},
+
+];
