0f77cf489786be58408b0e33c59bdfb04ddbea76
Yakubu Lute <yakubulute@outlook.com>
2025-11-08 10:34:04 +0000

    refactor: updated docker-compose yml file to comment out mongodb runners

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.neoverify.com',
+  apiUrl: 'https://api.neoverify.com/api',
+  aiUrl:'https://ai.neoverify.com/api',
   appName: 'NeoVerify Frontend',
   version: '1.0.0',
   features: {
